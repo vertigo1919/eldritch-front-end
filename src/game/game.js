6d6bc5a178d8f7cd4fd59fb6ca87fe80ecf70b73
Tvin9 <christopherharrison0606@gmail.com>
2026-03-10 16:11:14 +0000

    add proper scaling between max health and health bar

--- src/game/game.js
+++ src/game/game.js
@@ -1,16 +1,16 @@
-import Phaser from "phaser";
-import characterSceneSolo from "../scenes/characterSceneSolo";
-import HomePage from "../scenes/HomePage";
-import ComingSoon from "../scenes/ComingSoon";
+import Phaser from 'phaser';
+import characterSceneSolo from '../scenes/characterSceneSolo';
+import HomePage from '../scenes/HomePage';
+import ComingSoon from '../scenes/ComingSoon';
 
 export function createPhaserGame(parentId) {
-  return new Phaser.Game({
-    type: Phaser.AUTO,
-    parent: parentId,
-    width: 1280,
-    height: 720,
-    backgroundColor: "#0b0b10",
-    scene: [HomePage,characterSceneSolo, ComingSoon],
-    scale: { mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH },
-  });
+	return new Phaser.Game({
+		type: Phaser.AUTO,
+		parent: parentId,
+		width: 1280,
+		height: 720,
+		backgroundColor: '#0b0b10',
+		scene: [ComingSoon],
+		scale: { mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH },
+	});
 }
--- src/game/ui/HealthBar.jsx
+++ src/game/ui/HealthBar.jsx
@@ -1,9 +1,20 @@
-export default function createHealthBar(scene, x) {
-	scene.add.rectangle(x, 700, 205, 55, 0x000000);
-	const currentHealth = scene.add.rectangle(x, 700, 200, 50, 0xff0000);
+export default function createHealthBar(
+	scene,
+	x,
+	y,
+	maxHealth = 1000,
+	damage = 100,
+) {
+	scene.add.rectangle(x, 700, 205, 55, 0x000000).setOrigin(0.5);
+	const healthBar = scene.add
+		.rectangle(x, 700, 200, 50, 0xff0000)
+		.setOrigin(0.5);
+	const maxHealthBar = healthBar.width;
+	let remainingHealth = maxHealth;
+	let healthNum = scene.add.text(50, 50, `${remainingHealth}`).setOrigin(0.5);
 
 	const attackButton = scene.add
-		.text(scene.scale.width / 2, 650, 'Attack', {
+		.text(x, 650, 'Attack', {
 			fontSize: '32px',
 			color: '#FFFFFF',
 			backgroundColor: '#494949',
@@ -13,22 +24,26 @@ export default function createHealthBar(scene, x) {
 
 	attackButton.setInteractive({ useHandCursor: true });
 	attackButton.on('pointerdown', () => {
-		const damage = 30;
-		if (currentHealth.width - damage > 0) {
+		if (remainingHealth <= 0) return;
+		if (remainingHealth - damage > 0) {
+			remainingHealth = remainingHealth - damage;
 			scene.tweens.add({
-				targets: currentHealth,
-				width: currentHealth.width - damage,
+				targets: healthBar,
+				width: (remainingHealth / maxHealth) * maxHealthBar,
 				duration: 500,
 				ease: 'Sine.easeInOut',
 			});
-		} else if (currentHealth.width - damage <= 0) {
+		} else if (remainingHealth - damage <= 0) {
+			remainingHealth = 0;
+			console.log("You're dead!");
 			scene.tweens.add({
-				targets: currentHealth,
+				targets: healthBar,
 				width: 0,
 				duration: 500,
 				ease: 'Sine.easeInOut',
 			});
 		}
+		healthNum.setText(`${remainingHealth}`);
 		console.log('attack button is pressed');
 	});
 }
